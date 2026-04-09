2187723807aecfb083f81fb6235894dbd0124b33
jdbbjd <1090336995@qq.com>
2026-04-09 17:52:07 +0800

    update report generate logic

--- apps/test-engine/cli.py
+++ apps/test-engine/cli.py
@@ -3,47 +3,57 @@ import shutil
 import pytest
 import subprocess
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 PORT = int(os.getenv("PORT", 8888))
 REPORT_URL = f"http://127.0.0.1:{PORT}/index.html"
 
-def run():
-    print("🚀 开始执行测试...")
-
+def run(customer_id: str):
     # 1. 运行测试，生成 allure json 结果
+    print("🚀 开始执行测试...")
     pytest.main(["--alluredir=allure-results", "-vs"])
 
-    # 2. 生成 HTML 报告
-    print("\n📊 生成 HTML 报告...")
+    # 2. 准备报告目录
+    print("📃 准备报告文件夹")
+    report_dir = f"allure-reports/{customer_id}"
+    os.makedirs(report_dir, exist_ok=True)  # 已经存在不报错
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    tmp_report_dir = f"{report_dir}/{timestamp}_tmp"  # report 生成目录
+    final_html = f"{report_dir}/{timestamp}.html"  # 最终报告路径
+
+    # 3. 生成原始报告
+    print("\n📊 生成报告中...")
     subprocess.run([
-        "allure",
-        "generate",
+        "allure", "generate",
         "allure-results",
         "-o",
-        "allure-report-tmp",
+        tmp_report_dir,
         "--clean"
-    ])
-    print("✅ 报告已生成！")
+    ], check=True)
 
-    # 3. 更新正式报告
-    print("\n🚀 更新正式报告...")
-    if not os.path.exists("allure-report"):
-        shutil.copytree("allure-report-tmp", "allure-report")
-    else:
-        # 覆盖正式报告html
-        if os.path.exists("allure-report-tmp"):
-            shutil.copy("allure-report-tmp/index.html", "allure-report/index.html")
+    # 4. 生成最终报告
+    subprocess.run([
+        "allure-combine",
+        tmp_report_dir,
+        "--dest", report_dir
+    ], check=True)
 
-        # 复制报告数据文件夹
-        if os.path.exists("allure-report/data"):
-            shutil.rmtree("allure-report/data")
-            shutil.copytree("allure-report-tmp/data", "allure-report/data")
-    print(f"✅ 报告已更新！{REPORT_URL}")
+    # 重命名为带时间戳的文件名
+    generated_html = f"{report_dir}/complete.html"
+    os.rename(generated_html, final_html)
 
-    # 4.发送飞书消息
-    from notify.feishu import send_report
-    send_report(REPORT_URL)
+    # 清理临时目录
+    print("\n🧹 清理临时文件...")
+    shutil.rmtree(tmp_report_dir)
+    shutil.rmtree("allure-results")
+
+    # 5.发送飞书消息
+    report_url = f"http://127.0.0.1:{PORT}/{final_html}"
+    print(f"✅ 报告已生成！{report_url}")
+
+    # from notify.feishu import send_report
+    # send_report(REPORT_URL)
 
 if __name__ == "__main__":
-    run()
+    run("dianping")
--- apps/test-engine/core/test_web.py
+++ apps/test-engine/core/test_web.py
@@ -5,8 +5,5 @@ import pytest
 @allure.feature("Web UI 测试")
 @pytest.mark.parametrize("test_case", get_cases("web"),ids=lambda x: f"{x['name']}-{x['data'].get('user_id')}")
 def test_web_cases(test_case, web_runner):
-    enable = test_case['enable']
-    if not enable:
-        pytest.skip(f"用例未启用")
-
+    test_case['enable'] or pytest.skip(f"用例未启用")
     web_runner.run(test_case)
